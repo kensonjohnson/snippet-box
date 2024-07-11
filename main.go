55dd94109eaec64182a0ac13c93e1004fb99cf2f
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-07-11 18:28:40 -0500

    Setup base routes

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Create the servemux
+	mux := http.NewServeMux()
+
+	// Routes
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	// Start the server
+	log.Print("Server starting at http://localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+// Define a home handler function which write a byte slice
+// containing "Hello World" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form to create a snippet..."))
 }
