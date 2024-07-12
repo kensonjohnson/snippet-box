0a4e63f77df00b2c855d38773b742189c631cb66
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-07-11 21:01:44 -0500

    Add static file server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,14 +6,21 @@ import (
 )
 
 func main() {
+	// Create servemux
 	mux := http.NewServeMux()
+
+	// Serve static files
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// Define routes
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
+	// Start webserver
 	log.Print("starting server at http://localhost:4000")
-
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
