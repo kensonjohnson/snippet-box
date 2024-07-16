fc3c710a5049427fe6c2f458435c2f050e5cd9bc
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-07-16 16:02:10 -0500

    Setup pool connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,10 +18,20 @@ type application struct {
 func main() {
 	// Take in command line flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "user:password@tcp/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Setup logging
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	// Setup DB pool
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	// Dependency injection
 	app := &application{
@@ -27,7 +40,22 @@ func main() {
 
 	// Start webserver
 	logger.Info(fmt.Sprintf("starting server at http://localhost%s", *addr))
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
